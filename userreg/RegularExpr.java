aec530fdc0697a40e39c1b91388ca2499108d143
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-06-14 15:00:58 +0530

    Solution for UC-1

--- userreg/RegularExpr.java
+++ userreg/RegularExpr.java
@@ -1,7 +1,26 @@
 package com.bl.userreg;
 
+import java.util.Scanner;
+
 public class RegularExpr {
+	
+	public void userRegistration() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("The First Name is: ");
+		String fname=sc.nextLine();
+		String regexFname = "^[A-Z]{1}[a-z]{2,}";
+		 
+        if(fname.matches(regexFname)==true)
+        {
+         System.out.println("The first name "+fname+" is valid.");
+        }else
+        {
+         System.out.println("The first name "+fname+" is invalid.");
+        }
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to the user registration.");
+		RegularExpr obj=new RegularExpr();
+		obj.userRegistration();
 	}
 }
