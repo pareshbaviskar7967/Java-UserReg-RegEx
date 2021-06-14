32bcf632b0ca219ca338f7229c2a2c0f10c7dc19
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-06-14 15:37:10 +0530

    Solution for UC-3

--- userreg/RegularExpr.java
+++ userreg/RegularExpr.java
@@ -6,7 +6,7 @@ public class RegularExpr {
 	
 	public void firstName() {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("The First Name is: ");
+		System.out.println("Enter First Name is: ");
 		String fname=sc.nextLine();
 		String regexFname = "^[A-Z]{1}[a-z]{2,}";
 		 
@@ -21,7 +21,7 @@ public class RegularExpr {
 	
 	public void lastName() {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("The Last Name is: ");
+		System.out.println("Enter Last Name is: ");
 		String lname=sc.nextLine();
 		String regexFname = "^[A-Z]{1}[a-z]{2,}";
 		 
@@ -33,10 +33,26 @@ public class RegularExpr {
          System.out.println("The last name "+lname+" is invalid.");
         }
 	}
+	
+	public void eMail() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter eMail is: ");
+		String email=sc.nextLine();
+		String regexFname = "^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*@[A-Za-z0-9]+.[A-Za-z]{2,4}([.][A-Za-z]{2})*$";
+		 
+        if(email.matches(regexFname)==true)
+        {
+         System.out.println("The eMail "+email+" is valid.");
+        }else
+        {
+         System.out.println("The eMail "+email+" is invalid.");
+        }
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to the user registration.");
 		RegularExpr obj=new RegularExpr();
 		obj.firstName();
 		obj.lastName();
+		obj.eMail();
 	}
 }
